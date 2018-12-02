059339df56177728432100572515571d3cca9564
Maofkasopfkcafopasf <mohamedpadr94@gmail.com>
2018-12-02 15:38:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,3 +32,8 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
+
+
+
+
+client.login(process.env.BOT_TOKEN);
